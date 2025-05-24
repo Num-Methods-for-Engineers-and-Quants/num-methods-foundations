5f13e663c728704ffa2d3d2404274e1000c98038
Brice Nelson <bnelsonemail@icloud.com>
2025-05-24 01:10:13 -0400

    revised arguements to be passed to the function for Newton's Method

--- src/utils/root_finding/newtons_method.py
+++ src/utils/root_finding/newtons_method.py
@@ -1,14 +1,14 @@
 """src/utils/root_finding/newtons_method.py"""
 
 
-def newtons_method(guess, lst):
+def newtons_method(guess, coef):
     """
     Estimates the root of a function using Newton's method.
 
     inputs
     ------
     guess: initial guess for function root
-    lst:  list of function coefficients in a list in decreasing order
+    coef:  list of function coefficients in a list in decreasing order
 
     outputs
     -------
